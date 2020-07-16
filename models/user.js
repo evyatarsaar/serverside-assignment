bb0158725eb72d3328104995e575c9890f09adec
Evyatar Saar <evyatarsaar979@gmail.com>
2020-07-16 13:50:41 +0300

    passport

--- models/user.js
+++ models/user.js
@@ -1,17 +1,9 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new Schema(
   {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
     admin: {
       type: Boolean,
       default: false,
@@ -21,6 +13,8 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.plugin(passportLocalMongoose);
 
 var User = mongoose.model("User", userSchema);
 
